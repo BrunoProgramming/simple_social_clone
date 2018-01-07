dee30182fe74ee5599c1e6d554c27bd649e91a12
BrunoProgramming <brunodengchn@gmail.com>
2018-01-07 23:23:22 +0800

    merge with style codes
    merge with style codes

---
+++ soulfeeling/static/js/login.js
@@ -0,0 +1,12 @@
+﻿function Registor() {
+    document.getElementById("loginBtn").style.display = "none";
+    document.getElementById("confirm").style.display = "";
+    document.getElementById("loginTransfer").style.display = "";
+    document.getElementById("passWord").setAttribute("placeholder", "设置密码*");
+}
+
+function load() {
+    document.getElementById("loginBtn").style.display = "";
+    document.getElementById("confirm").style.display = "none";
+    document.getElementById("loginTransfer").style.display = "none";
+}
