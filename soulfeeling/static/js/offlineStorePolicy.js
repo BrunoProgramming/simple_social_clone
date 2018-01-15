ae25250ce1513d039144c110346413bd5b01a3e5
BrunoProgramming <brunodengchn@gmail.com>
2018-01-16 02:11:11 +0800

    refactor

---
+++ soulfeeling/static/js/offlineStorePolicy.js
@@ -0,0 +1,45 @@
+function changeNewStore(imgName)
+{
+    document.getElementById("offlineStoreFirst").style.display = "none";
+    document.getElementById("offlineStoreSecond").style.display = "";
+    document.getElementById("offlineStoreThird").style.display = "none";
+    document.getElementById("offlineStoreFourth").style.display = "none";
+
+    document.getElementById("navSecond").classList.remove('active');
+    document.getElementById("navThird").classList.remove('active');
+    document.getElementById("navFirst").classList.add('active');
+
+    document.getElementById("linkContent").textContent="最新门店";
+}
+
+function changeAllStore(imgName)
+{
+    document.getElementById("offlineStoreFirst").style.display = "";
+    document.getElementById("offlineStoreSecond").style.display = "none";
+    document.getElementById("offlineStoreThird").style.display = "none";
+    document.getElementById("offlineStoreFourth").style.display = "none";
+
+    document.getElementById("navFirst").classList.remove('active');
+    document.getElementById("navThird").classList.remove('active');
+    document.getElementById("navSecond").classList.add('active');
+
+    document.getElementById("linkContent").textContent="全部门店";
+}
+
+function changePolicyStore(imgName)
+{
+    document.getElementById("offlineStoreFirst").style.display = "none";
+    document.getElementById("offlineStoreSecond").style.display = "none";
+    document.getElementById("offlineStoreThird").style.display = "none";
+    document.getElementById("offlineStoreFourth").style.display = "";
+
+    document.getElementById("navSecond").classList.remove('active');
+    document.getElementById("navFirst").classList.remove('active');
+    document.getElementById("navThird").classList.add('active');
+
+    document.getElementById("linkContent").textContent="优惠政策";
+}
+
+window.onload = function() {
+    changePolicyStore("shop");
+  };
