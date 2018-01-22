0c86be038b1f26bc41bf1ea62d409c9a9822837c
BrunoProgramming <brunodengchn@gmail.com>
2018-01-22 18:09:47 +0800

    remove dj-database-url

--- soulfeeling/soulfeeling/settings.py
+++ soulfeeling/soulfeeling/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
@@ -129,9 +130,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 LOGIN_REDIRECT_URL = "groups:secondPageContent"
 LOGOUT_REDIRECT_URL = "accounts:user_login"
 
-import dj-database-url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 
 STATIC_URL = '/static/'
